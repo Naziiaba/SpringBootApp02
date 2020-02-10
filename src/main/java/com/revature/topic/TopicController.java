78da5f7c7eecfd88669b52abbab8652011ca63db
Zia pmr <gernersnc@gmail.com>
2020-02-10 15:36:25 -0500

    added BusinessService. Getting particular resources

--- src/main/java/com/revature/topic/TopicController.java
+++ src/main/java/com/revature/topic/TopicController.java
@@ -3,17 +3,24 @@ package com.revature.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
+	
+	@Autowired 
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-				new Topic("spring", "spring framework", "spring description"),
-				new Topic("java", "java framework", "description2"),
-				new Topic("python", "framework", "desc3")
-				);
+		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
 	}
 }
--- src/main/java/com/revature/topic/TopicService.java
+++ src/main/java/com/revature/topic/TopicService.java
@@ -1,5 +1,23 @@
 package com.revature.topic;
 
-public class TopicService {
+import java.util.Arrays;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = Arrays.asList(
+			new Topic("spring", "spring framework", "spring description"),
+			new Topic("java", "java framework", "description2"),
+			new Topic("python", "framework", "desc3")
+			);
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
 }
