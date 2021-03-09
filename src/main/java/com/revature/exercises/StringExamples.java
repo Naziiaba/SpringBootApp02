4445d69b9714f2ae7ce6748d8ebb3517324f5b8c
Zia <gernersnc@gmail.com>
2021-03-09 13:24:33 -0500

    replace

--- src/main/java/com/revature/exercises/StringExamples.java
+++ src/main/java/com/revature/exercises/StringExamples.java
@@ -14,10 +14,11 @@ public class StringExamples {
 		StringBuffer sb = new StringBuffer();
 		sb.append(numbers);
 		String str3 = sb.toString();
-		System.out.println("A new string contains " + str3);
+		String res = str3.replace("2468", "Computing");
+		System.out.println("A new string contains " + res);
 
 		String str4 = "Programming ";
-		String joining2Strings = str4.concat(str3);
+		String joining2Strings = str4.concat(res);
 		System.out.print("Joined Strings are: " + joining2Strings);
 		
 		//These following Strings need to be separated
